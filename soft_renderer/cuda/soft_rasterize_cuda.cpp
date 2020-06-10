9fedc49ca37adcb55297711cee17620316113135
Kunyao Chen <funtionyao32@gmail.com>
2020-06-09 18:38:36 -0700

    replace AT_CHECK with TORCH_CHECK (pytorch1.5)

--- soft_renderer/cuda/soft_rasterize_cuda.cpp
+++ soft_renderer/cuda/soft_rasterize_cuda.cpp
@@ -30,7 +30,7 @@ std::vector<at::Tensor> forward_soft_rasterize_cuda(
 std::vector<at::Tensor> backward_soft_rasterize_cuda(
         at::Tensor faces,
         at::Tensor textures,
-        at::Tensor soft_colors,        
+        at::Tensor soft_colors,
         at::Tensor faces_info,
         at::Tensor aggrs_info,
         at::Tensor grad_faces,
@@ -51,8 +51,8 @@ std::vector<at::Tensor> backward_soft_rasterize_cuda(
 
 // C++ interface
 
-#define CHECK_CUDA(x) AT_CHECK(x.type().is_cuda(), #x " must be a CUDA tensor")
-#define CHECK_CONTIGUOUS(x) AT_CHECK(x.is_contiguous(), #x " must be contiguous")
+#define CHECK_CUDA(x) TORCH_CHECK(x.type().is_cuda(), #x " must be a CUDA tensor")
+#define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
 #define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
 
 
@@ -81,12 +81,12 @@ std::vector<at::Tensor> forward_soft_rasterize(
     CHECK_INPUT(aggrs_info);
     CHECK_INPUT(soft_colors);
 
-    return forward_soft_rasterize_cuda(faces, textures, 
-                                       faces_info, aggrs_info, 
-                                       soft_colors, 
-                                       image_size, near, far, eps, 
+    return forward_soft_rasterize_cuda(faces, textures,
+                                       faces_info, aggrs_info,
+                                       soft_colors,
+                                       image_size, near, far, eps,
                                        sigma_val, func_id_dist, dist_eps,
-                                       gamma_val, func_id_rgb, func_id_alpha, 
+                                       gamma_val, func_id_rgb, func_id_alpha,
                                        texture_sample_type, double_side);
 }
 
@@ -122,12 +122,12 @@ std::vector<at::Tensor> backward_soft_rasterize(
     CHECK_INPUT(grad_textures);
     CHECK_INPUT(grad_soft_colors);
 
-    return backward_soft_rasterize_cuda(faces, textures, soft_colors, 
-                                        faces_info, aggrs_info, 
-                                        grad_faces, grad_textures, grad_soft_colors, 
-                                        image_size, near, far, eps, 
+    return backward_soft_rasterize_cuda(faces, textures, soft_colors,
+                                        faces_info, aggrs_info,
+                                        grad_faces, grad_textures, grad_soft_colors,
+                                        image_size, near, far, eps,
                                         sigma_val, func_id_dist, dist_eps,
-                                        gamma_val, func_id_rgb, func_id_alpha, 
+                                        gamma_val, func_id_rgb, func_id_alpha,
                                         texture_sample_type, double_side);
 }
 
